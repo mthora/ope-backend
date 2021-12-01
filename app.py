cb7bf3018999535e05a93a67a882065f79160e96
Matheus Teixeira <matheus.teixeira@itenabler.com.br>
2021-12-01 11:22:23 -0300

    teste

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from src.main.config import app, api
 from src.infra.config import *
 from src.infra.db_entities import *
 from src.main.utils import hash_password
+from flask import Flask
+from flask_restx import Api
+from src.main.routes import user_namespace, product_namespace, product_order_namespace, order_namespace
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '00fe747c35'
+app.config['UPLOAD_FOLDER'] = 'src\img'
+
+api = Api(
+    title="Talos RMS",
+    version='1.0',
+    description="Restaurant Management System"
+)
+
+api.add_namespace(user_namespace, path='/users')
+api.add_namespace(product_namespace, path='/products')
+api.add_namespace(product_order_namespace, path='/products_orders')
+api.add_namespace(order_namespace, path='/orders')
 
 if __name__ == "__main__":
     api.init_app(app)
